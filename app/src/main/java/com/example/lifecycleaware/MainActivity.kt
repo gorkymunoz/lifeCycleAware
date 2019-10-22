8fdf829aa5e8737ea455e296545a8c199cf5e219
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-10-22 11:40:00 -0500

    Se añadio el observador para el LiveData

--- app/src/main/java/com/example/lifecycleaware/MainActivity.kt
+++ app/src/main/java/com/example/lifecycleaware/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lifecycleaware
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,8 +19,15 @@ class MainActivity : AppCompatActivity() {
             .get(MainActivityViewModel::class.java)
 
         val myRandomNumber = model.getNumber()
-        tvNumber.text = myRandomNumber
-        Log.d(TAG, "Random Number Set")
+
+        myRandomNumber.observe(this,Observer<String>{ number ->
+            tvNumber.text = number
+            Log.d(TAG, "Random Number Set")
+        })
+
+        bRandom.setOnClickListener {
+            model.createNumber()
+        }
     }
 
     companion object{
